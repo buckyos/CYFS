0ebbe7dc3907ad4e79e26d1a3d96131074537300
weiqiushi <weiqiushi@buckyos.com>
2022-07-06 15:22:16 +0800

    Implement trait Termination for BuckyError, so that the main function returns BuckyResult correctly

--- src/component/cyfs-base/src/base/error.rs
+++ src/component/cyfs-base/src/base/error.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::error::Error;
 use std::fmt::{self, Debug, Display};
 use std::io::ErrorKind;
+use std::process::{ExitCode, Termination};
 
 // 系统的内置error code范围 [BUCKY_SYSTEM_ERROR_CODE_START, BUCKY_SYSTEM_ERROR_CODE_END)
 pub const BUCKY_SYSTEM_ERROR_CODE_START: u16 = 0;
@@ -1209,6 +1210,12 @@ impl Into<ErrorKind> for BuckyErrorCode {
 
             _ => ErrorKind::Other,
         }
+    }
+}
+
+impl Termination for BuckyError {
+    fn report(self) -> ExitCode {
+        ExitCode::from(self.code.into_u8())
     }
 }
 
