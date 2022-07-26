1b8aee0d2698ad1753fbabb568ec916c6577a43a
xiaohongjin <xiaohongjin@buckyos.com>
2022-07-26 19:26:55 +0800

    delete useless state in uploadsession
    Signed-off-by: xiaohongjin <xiaohongjin@buckyos.com>

--- src/component/cyfs-bdt/src/utils/event_utils.rs
+++ src/component/cyfs-bdt/src/utils/event_utils.rs
@@ -1,12 +1,13 @@
 
-use std::{sync::{Mutex}, collections::BTreeMap};
+use std::{sync::{Mutex}, collections::{BTreeSet}};
 
 use cyfs_base::*;
 use crate::{
-    stack::{Stack} 
+    stack::{Stack}, 
 };
-use crate::{
-    ndn::{NdnEventHandler, DefaultNdnEventHandler}, 
+use crate::ndn::{
+    DefaultNdnEventHandler, NdnEventHandler,
+    // event::*,
     // ChunkTask,
     // chunk::ChunkDownloadConfig,
     channel::{
@@ -16,20 +17,25 @@ use crate::{
     }, 
 };
 
-pub struct RedirectHandle {}
+pub struct RedirectHandle {
+    redirect_target: DeviceId,
+}
 
 #[async_trait::async_trait]
 impl NdnEventHandler for RedirectHandle {
     async fn on_newly_interest(&self, stack: &Stack, interest: &Interest, from: &Channel) -> BuckyResult<()> {
-        let session = UploadSession::redirect(interest.chunk.clone(), 
-                                              interest.session_id.clone(), 
-                                              interest.prefer_type.clone(), 
-                                              from.clone(), 
-                                              DeviceId::default(), 
-                                              String::default());
+        let resp_interest = 
+            RespInterest { session_id: interest.session_id.clone(), 
+                           chunk: interest.chunk.clone(), 
+                           err: BuckyErrorCode::Redirect,
+                           redirect: Some(self.redirect_target.clone()),
+                           redirect_referer: Some(String::default()),
+                           to: None,
+                        };
+        from.resp_interest(resp_interest);
 
-        let _ = from.upload(session.clone());
-        session.on_interest(interest)
+        Ok(())
+
     }
 
     fn on_unknown_piece_data(&self, _stack: &Stack, _piece: &PieceData, _from: &Channel) -> BuckyResult<DownloadSession> {
@@ -41,7 +47,7 @@ impl NdnEventHandler for RedirectHandle {
 pub struct ForwardEventHandle {
     target: DeviceId,
     default_handle: DefaultNdnEventHandler,
-    session_cache: Mutex<BTreeMap<u32, bool>>
+    session_cache: Mutex<BTreeSet<u32>>
 }
 
 impl ForwardEventHandle {
@@ -49,7 +55,7 @@ impl ForwardEventHandle {
         Self { 
             target,
             default_handle: DefaultNdnEventHandler::new(),
-            session_cache: Mutex::new(BTreeMap::new()),
+            session_cache: Mutex::new(BTreeSet::new()),
         }
     }
 }
@@ -62,22 +68,21 @@ impl NdnEventHandler for ForwardEventHandle {
                 {
                     let session_cache = &mut *self.session_cache.lock().unwrap();
 
-                    if session_cache.contains_key(&interest.session_id.value()) {
+                    if session_cache.contains(&interest.session_id.value()) {
                         return Ok(());
                     }
 
-                    session_cache.insert(interest.session_id.value(), true);
+                    session_cache.insert(interest.session_id.value());
                 }
 
-                let forward_session = 
-                    UploadSession::forward(interest.chunk.clone(),
-                                           interest.session_id.clone(), 
-                                           interest.prefer_type.clone(), 
-                                           from.clone(),
-                                           target,
-                                           String::default());
-    
-                let _ = forward_session.on_interest(interest);
+                let interest = 
+                    Interest { session_id: interest.session_id.clone(), 
+                               chunk: interest.chunk.clone(),
+                               prefer_type: interest.prefer_type.clone(),
+                               from: Some(from.remote().clone()),
+                               referer: Some(String::default()),};
+
+                target.interest(interest);
                 Ok(())
                 
             } else {
