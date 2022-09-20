6de5fe93ec8e25d0904bb60e915c3d7a5ef8f5a7
liyaxing <liyaxing@buckyos.com>
2022-09-21 00:50:07 +0800

    Improve RequestGlobalStatePath dec_id field design and format/parse methods

--- src/component/cyfs-lib/src/prelude/global_state_common.rs
+++ src/component/cyfs-lib/src/prelude/global_state_common.rs
@@ -31,7 +31,7 @@ pub struct RequestGlobalStatePath {
     // root or dec-root object-id
     pub global_state_root: Option<RequestGlobalStateRoot>,
 
-    // target DEC，if is none then equal as system dec-id
+    // target DEC，if is none then equal as source dec-id
     pub dec_id: Option<ObjectId>,
 
     // inernal path of global-state, without the dec-id segment
@@ -49,7 +49,6 @@ impl fmt::Debug for RequestGlobalStatePath {
     }
 }
 
-
 impl RequestGlobalStatePath {
     pub fn new(dec_id: Option<ObjectId>, req_path: Option<impl Into<String>>) -> Self {
         Self {
@@ -61,7 +60,10 @@ impl RequestGlobalStatePath {
     }
 
     pub fn new_system_dec(req_path: Option<impl Into<String>>) -> Self {
-        Self::new(None, req_path)
+        Self::new(
+            Some(cyfs_core::get_system_dec_app().object_id().to_owned()),
+            req_path,
+        )
     }
 
     pub fn category(&self) -> GlobalStateCategory {
@@ -78,13 +80,6 @@ impl RequestGlobalStatePath {
         }
     }
 
-    pub fn dec(&self) -> &ObjectId {
-        match &self.dec_id {
-            Some(id) => id,
-            None => cyfs_core::get_system_dec_app().object_id(),
-        }
-    }
-
     /*
     The first paragraph is optional root-state/local-cache, default root-state
     The second paragraph is optional current/root:{root-id}/dec-root:{dec-root-id}, default is current
@@ -98,85 +93,79 @@ impl RequestGlobalStatePath {
             .filter(|seg| !seg.is_empty())
             .collect();
 
-        if segs.is_empty() {
-            let msg = format!("invalid request path! {}", req_path);
-            error!("{}", msg);
-            return Err(BuckyError::new(BuckyErrorCode::InvalidParam, msg));
-        }
-
         let mut index = 0;
-        let seg = segs[index];
-        let global_state_category = match seg {
-            "root-state" => {
-                index += 1;
-                Some(GlobalStateCategory::RootState)
-            }
-            "local-cache" => {
-                index += 1;
-                Some(GlobalStateCategory::LocalCache)
-            }
+        let global_state_category = if index < segs.len() {
+            let seg = segs[index];
+            match seg {
+                "root-state" => {
+                    index += 1;
+                    Some(GlobalStateCategory::RootState)
+                }
+                "local-cache" => {
+                    index += 1;
+                    Some(GlobalStateCategory::LocalCache)
+                }
 
-            _ => None,
+                _ => None,
+            }
+        } else {
+            None
         };
 
-        if index >= segs.len() {
-            let msg = format!("invalid request path! {}", req_path);
-            error!("{}", msg);
-            return Err(BuckyError::new(BuckyErrorCode::InvalidParam, msg));
-        }
+        let global_state_root = if index < segs.len() {
+            let seg = segs[index];
+            match seg {
+                "current" => {
+                    index += 1;
+                    None
+                }
+                _ if seg.starts_with("root:") => {
+                    index += 1;
 
-        let seg = segs[index];
-        let global_state_root = match seg {
-            "current" => {
-                index += 1;
-                None
+                    let id = seg.strip_prefix("root:").unwrap();
+                    let root = ObjectId::from_str(&id).map_err(|e| {
+                        let msg = format!("invalid req_path's root id: {}, {}", seg, e);
+                        error!("{msg}");
+                        BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
+                    })?;
+
+                    Some(RequestGlobalStateRoot::GlobalRoot(root))
+                }
+                _ if seg.starts_with("dec-root:") => {
+                    index += 1;
+
+                    let id = seg.strip_prefix("dec-root:").unwrap();
+                    let root = ObjectId::from_str(&id).map_err(|e| {
+                        let msg = format!("invalid req_path's dec root id: {}, {}", seg, e);
+                        error!("{msg}");
+                        BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
+                    })?;
+
+                    Some(RequestGlobalStateRoot::DecRoot(root))
+                }
+                _ => None,
             }
-            _ if seg.starts_with("root:") => {
-                index += 1;
-
-                let id = seg.strip_prefix("root:").unwrap();
-                let root = ObjectId::from_str(&id).map_err(|e| {
-                    let msg = format!("invalid req_path's root id: {}, {}", seg, e);
-                    error!("{msg}");
-                    BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
-                })?;
-
-                Some(RequestGlobalStateRoot::GlobalRoot(root))
-            }
-            _ if seg.starts_with("dec-root:") => {
-                index += 1;
-
-                let id = seg.strip_prefix("dec-root:").unwrap();
-                let root = ObjectId::from_str(&id).map_err(|e| {
-                    let msg = format!("invalid req_path's dec root id: {}, {}", seg, e);
-                    error!("{msg}");
-                    BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
-                })?;
-
-                Some(RequestGlobalStateRoot::DecRoot(root))
-            }
-            _ => None,
+        } else {
+            None
         };
 
-        if index >= segs.len() {
-            let msg = format!("invalid request path param! param={}", req_path);
-            error!("{}", msg);
-            return Err(BuckyError::new(BuckyErrorCode::InvalidParam, msg));
-        }
-
-        let seg = segs[index];
-        let dec_id = match seg {
-            "system" => None,
-            _ => {
+        let dec_id = if index < segs.len() {
+            // 如果第一段是object_id，那么认为是dec_id
+            let seg = segs[index];
+            if OBJECT_ID_BASE58_RANGE.contains(&seg.len()) {
                 let dec_id = ObjectId::from_str(seg).map_err(|e| {
                     let msg = format!("invalid req_path's dec root id: {}, {}", seg, e);
                     error!("{msg}");
                     BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
                 })?;
+                index += 1;
                 Some(dec_id)
+            } else {
+                None
             }
+        } else {
+            None
         };
-        index += 1;
 
         let req_path = if index < segs.len() {
             let path = segs[index..].join("/");
@@ -203,18 +192,10 @@ impl RequestGlobalStatePath {
             segs.push(Cow::Owned(root.to_string()));
         }
 
-        let seg = match &self.dec_id {
-            Some(id) => {
-                if id == cyfs_core::get_system_dec_app().object_id() {
-                    Cow::Borrowed("system")
-                } else {
-                    Cow::Owned(id.to_string())
-                }
-            }
-            None => Cow::Borrowed("system"),
+        if let Some(id) = &self.dec_id {
+            let seg = Cow::Owned(id.to_string());
+            segs.push(seg);
         };
-
-        segs.push(seg);
 
         if let Some(path) = &self.req_path {
             segs.push(Cow::Borrowed(path.trim_start_matches('/')));
@@ -258,7 +239,7 @@ mod test {
             global_state_category: None,
             global_state_root: None,
             dec_id: None,
-            req_path: Some("/a/b".to_owned()),
+            req_path: Some("/a/b/".to_owned()),
         };
 
         let s = root.format_string();
@@ -284,7 +265,7 @@ mod test {
         let r = RequestGlobalStatePath::parse(&s).unwrap();
         assert_eq!(root, r);
 
-        root.req_path = Some("/a".to_owned());
+        root.req_path = Some("/a/".to_owned());
         let s = root.format_string();
         println!("{}", s);
         let r = RequestGlobalStatePath::parse(&s).unwrap();
@@ -294,6 +275,21 @@ mod test {
         let s = root.format_string();
         println!("{}", s);
         let r = RequestGlobalStatePath::parse(&s).unwrap();
-        assert_eq!(r.dec_id, None);
+        assert_eq!(
+            r.dec_id,
+            Some(cyfs_core::get_system_dec_app().object_id().to_owned())
+        );
+
+        let root = RequestGlobalStatePath {
+            global_state_category: None,
+            global_state_root: None,
+            dec_id: None,
+            req_path: None,
+        };
+
+        let s = root.format_string();
+        println!("{}", s);
+        let r = RequestGlobalStatePath::parse(&s).unwrap();
+        assert_eq!(root, r);
     }
 }
