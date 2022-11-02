965a7ef730410e113051811c123adaece07e0d79
liyaxing <liyaxing@buckyos.com>
2022-11-02 20:18:24 +0800

    Remove dsg related projects from solution

--- src/tests/dsg-test/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod miner;
-mod device;
-pub mod ignore_witness;
-pub mod non_witness;
-pub use device::*;
--- src/tests/dsg-test/src/miner/mod.rs
+++
@@ -1,9 +0,0 @@
-mod client;
-mod service;
-mod non_witness;
-mod miner;
-
-pub use service::*;
-pub use client::*;
-pub use non_witness::*;
-pub use miner::*;
