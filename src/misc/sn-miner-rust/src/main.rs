1691c2b4183da3d2a7111a23a2d316895ab5ec05
xiaohongjin <xiaohongjin@buckyos.com>
2022-11-11 16:37:00 +0800

    Add panic-report in sn-miner-rust.
    Signed-off-by: xiaohongjin <xiaohongjin@buckyos.com>

--- src/misc/sn-miner-rust/src/main.rs
+++ src/misc/sn-miner-rust/src/main.rs
@@ -1,11 +1,13 @@
 mod init_log;
 
+use std::collections::LinkedList;
 use std::io::{Read, Write};
 use std::path::Path;
 use std::str::FromStr;
 
 use cyfs_base::*;
 use cyfs_bdt::{sn::service::*, ReceiptWithSignature, SnServiceReceipt};
+use cyfs_debug::DebugConfig;
 
 const APP_NAME: &str = "sn-miner";
 
@@ -63,9 +65,27 @@ fn get_ep_list() -> Vec<Endpoint> {
     eps
 }
 
+fn get_debug_config(key: &str) -> Option<&str> {
+    let mut keys: LinkedList<&str> = key.split('.').collect();
+
+    let mut first = match keys.pop_front() {
+        Some(first) => DebugConfig::get_config(first),
+        None => None,
+    }?;
+
+    while let Some(key) = keys.pop_front() {
+        first = first.get(key)?;
+    }
+
+    first.as_str()
+}
+
 #[async_std::main]
 async fn main() {
     cyfs_util::process::check_cmd_and_exec(APP_NAME);
+
+    let report_url = get_debug_config("report.dingtalk");
+
     cyfs_debug::CyfsLoggerBuilder::new_app(APP_NAME)
         .level("info")
         .console("warn")
@@ -73,10 +93,15 @@ async fn main() {
         .unwrap()
         .start();
 
-    cyfs_debug::PanicBuilder::new(APP_NAME, APP_NAME)
-        .exit_on_panic(true)
-        .build()
-        .start();
+    if let Some(report_url) = report_url {
+        cyfs_debug::PanicBuilder::new(APP_NAME, APP_NAME)
+        .http_bug_report(report_url)
+    } else {
+        cyfs_debug::PanicBuilder::new(APP_NAME, APP_NAME)
+    }
+    .exit_on_panic(true)
+    .build()
+    .start();
 
     let data_folder = ::cyfs_util::get_app_data_dir(APP_NAME);
     let (local_device, private_key) = if let Ok(loaded_device) =
