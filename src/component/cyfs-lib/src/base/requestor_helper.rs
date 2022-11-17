b9c78ccd3f1a6f780d43a5d0155333b9ab4a732e
liyaxing <liyaxing@buckyos.com>
2022-11-17 22:45:40 +0800

    Add percent codec support with some encode and decode helper funcs

--- src/component/cyfs-lib/src/base/requestor_helper.rs
+++ src/component/cyfs-lib/src/base/requestor_helper.rs
@@ -292,6 +292,25 @@ impl RequestorHelper {
         http_req.insert_header(name, &header_list[..]);
     }
 
+    pub fn insert_headers_with_encoding<T>(http_req: &mut Request, name: &str, list: &Vec<T>)
+    where
+        T: ToString,
+    {
+        let header_list: Vec<HeaderValue> = list
+            .iter()
+            .map(|item| {
+                let item = item.to_string();
+                let ret = percent_encoding::utf8_percent_encode(
+                    &item,
+                    percent_encoding::NON_ALPHANUMERIC,
+                );
+                HeaderValue::from_bytes(ret.to_string().into_bytes()).unwrap()
+            })
+            .collect();
+
+        http_req.insert_header(name, &header_list[..]);
+    }
+
     pub fn trans_status_code(code: StatusCode) -> BuckyErrorCode {
         if code.is_success() {
             return BuckyErrorCode::Ok;
@@ -351,7 +370,7 @@ impl RequestorHelper {
         Ok(ret)
     }
 
-    pub fn decode_utf8(name: &str, value: &str) -> BuckyResult<String> {
+    pub fn decode_utf8<'a>(name: &'a str, value: &'a str) -> BuckyResult<Cow<'a, str>> {
         let decoded_value = percent_encoding::percent_decode_str(&value);
         let value = decoded_value.decode_utf8().map_err(|e| {
             let msg = format!("invalid header format: {} = {} {}", name, value, e);
@@ -359,21 +378,30 @@ impl RequestorHelper {
             BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
         })?;
 
-        Ok(value.to_string())
+        Ok(value)
     }
 
-    pub fn decode_optional_header_with_utf8_decoding<R>(
+    pub fn decode_optional_header_with_utf8_decoding<T, R>(
         req: &R,
         name: impl Into<HeaderName>,
-    ) -> BuckyResult<Option<String>>
+    ) -> BuckyResult<Option<T>>
     where
         R: HeaderOp,
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Display,
     {
         let mut ret = None;
         let name: HeaderName = name.into();
         if let Some(header) = req.header(&name) {
-            let value = header.last().as_str();
-            ret = Some(Self::decode_utf8(name.as_str(), value)?);
+            let value = Self::decode_utf8(name.as_str(), header.last().as_str())?;
+            let value = T::from_str(&value).map_err(|e| {
+                let msg = format!("invalid header format: {} = {} {}", name, value, e);
+                error!("{}", msg);
+
+                BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
+            })?;
+
+            ret = Some(value);
         }
 
         Ok(ret)
@@ -397,12 +425,14 @@ impl RequestorHelper {
         }
     }
 
-    pub fn decode_header_with_utf8_decoding<R>(
+    pub fn decode_header_with_utf8_decoding<T, R>(
         req: &R,
         name: impl Into<HeaderName>,
-    ) -> BuckyResult<String>
+    ) -> BuckyResult<T>
     where
         R: HeaderOp,
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Display,
     {
         let name: HeaderName = name.into();
         match Self::decode_optional_header_with_utf8_decoding(req, &name)? {
@@ -453,6 +483,57 @@ impl RequestorHelper {
     {
         let name: HeaderName = name.into();
         match Self::decode_optional_headers(req, &name)? {
+            Some(v) => Ok(v),
+            None => {
+                let msg = format!("header not found: {}", name);
+                error!("{}", msg);
+
+                Err(BuckyError::new(BuckyErrorCode::NotFound, msg))
+            }
+        }
+    }
+
+    pub fn decode_optional_headers_with_utf8_decoding<T, R>(
+        req: &R,
+        name: impl Into<HeaderName>,
+    ) -> BuckyResult<Option<Vec<T>>>
+    where
+        R: HeaderOp,
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Display,
+    {
+        let name: HeaderName = name.into();
+        if let Some(headers) = req.header(&name) {
+            let mut rets = Vec::new();
+            for header in headers {
+                let value = Self::decode_utf8(name.as_str(), header.as_str())?;
+                let value = T::from_str(&value).map_err(|e| {
+                    let msg = format!("invalid header format: {} = {}, {}", name, value, e);
+                    error!("{}", msg);
+
+                    BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
+                })?;
+
+                rets.push(value);
+            }
+
+            Ok(Some(rets))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn decode_headers_with_utf8_decoding<T, R>(
+        req: &R,
+        name: impl Into<HeaderName>,
+    ) -> BuckyResult<Vec<T>>
+    where
+        R: HeaderOp,
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Display,
+    {
+        let name: HeaderName = name.into();
+        match Self::decode_optional_headers_with_utf8_decoding(req, &name)? {
             Some(v) => Ok(v),
             None => {
                 let msg = format!("header not found: {}", name);
@@ -644,6 +725,22 @@ impl RequestorHelper {
         Ok(value)
     }
 
+    pub fn decode_url_param_with_utf8_decoding<T>(k: Cow<str>, v: Cow<str>) -> BuckyResult<T>
+    where
+        T: FromStr,
+        <T as FromStr>::Err: std::fmt::Display,
+    {
+        let value = Self::decode_utf8(&k, &v)?;
+        let value = T::from_str(&value).map_err(|e| {
+            let msg = format!("invalid url param: {} = {}, {}", k, value, e);
+            error!("{}", msg);
+
+            BuckyError::new(BuckyErrorCode::InvalidFormat, msg)
+        })?;
+
+        Ok(value)
+    }
+
     // 参数使用,分隔
     pub fn decode_url_param_list<T>(k: Cow<str>, v: Cow<str>) -> BuckyResult<Vec<T>>
     where
@@ -676,7 +773,7 @@ impl RequestorHelper {
         if let Some(value) = value {
             let v: String =
                 percent_encoding::utf8_percent_encode(value, percent_encoding::NON_ALPHANUMERIC)
-                    .collect();
+                    .to_string();
             req.insert_header(name, v);
         }
     }
@@ -693,9 +790,8 @@ impl RequestorHelper {
     where
         R: HeaderOp,
     {
-        let v: String =
-            percent_encoding::utf8_percent_encode(value, percent_encoding::NON_ALPHANUMERIC)
-                .collect();
+        let v = percent_encoding::utf8_percent_encode(value, percent_encoding::NON_ALPHANUMERIC)
+            .to_string();
         req.insert_header(name, v);
     }
 
@@ -793,7 +889,7 @@ impl RequestorHelper {
                     };
 
                     Ok(dec_id)
-                } else{
+                } else {
                     Ok(None)
                 }
             }
