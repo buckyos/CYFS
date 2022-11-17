896b39bac871c05562d4a5574e03fdac9907a0d9
tsukasa <puotosssa@live.com>
2022-11-17 16:06:25 +0800

    Impl: File raw cache for chunk

--- src/component/cyfs-bdt/src/ndn/chunk/cache/raw_cache/file.rs
+++ src/component/cyfs-bdt/src/ndn/chunk/cache/raw_cache/file.rs
@@ -1,88 +1,276 @@
-// use std::{
-//     sync::{Arc, RwLock}, usize, 
-//     io::SeekFrom, 
-// };
-// use async_std::{
-//     pin::Pin, 
-//     task::{Context, Poll},
-//     fs::File
-// };
-// use cyfs_base::*;
-// use cyfs_util::{
-//     AsyncWriteWithSeek, 
-//     AsyncReadWithSeek, 
-//     SyncWriteWithSeek, 
-//     SyncReadWithSeek
-// };
-// use super::{
-//     common::*, 
-//     manager::*
-// };
+use std::{
+    sync::{Arc, RwLock}, usize, 
+    io::SeekFrom, 
+    ops::Range, 
+    path::{Path, PathBuf}
+};
+use async_std::{
+    pin::Pin, 
+    task::{self, Context, Poll},
+    fs::{self, File},
+    io::{prelude::SeekExt}
+};
+use cyfs_base::*;
+use cyfs_util::{
+    AsyncWriteWithSeek, 
+    AsyncReadWithSeek, 
+    SyncWriteWithSeek, 
+    SyncReadWithSeek
+};
+use crate::{
+    types::*
+};
+use super::{
+    common::*, 
+};
 
-// enum CacheState {
-//     Creating,
-//     Created(File), 
-// }
+enum CacheState {
+    Creating(StateWaiter),
+    Created(File), 
+    Error(BuckyErrorCode, String)
+}
 
-// struct CacheImpl {
-//     state: 
-//     capacity: usize
-// }
+struct CacheImpl {
+    state: RwLock<CacheState>, 
+    path: PathBuf,
+    range: Range<u64>, 
+    to_remove: bool
+}
 
-// impl CacheImpl {
-//     fn capacity(&self) -> usize {
-//         self.capacity
-//     } 
-// }
 
-// impl Drop for FileCache {
-//     fn drop(&mut self) {
-//         task::spawn(async move {
+impl Drop for FileCache {
+    fn drop(&mut self) {
+        if self.0.to_remove {
+            let to_remove = {
+                let state = &mut *self.0.state.write().unwrap();
+                let to_remove = match state {
+                    CacheState::Created(_) => true, 
+                    _ => false
+                };
+                *state = CacheState::Error(BuckyErrorCode::NotInit, "closed".to_owned());
+                to_remove
+            };
+            
+            if to_remove {
+                let path = self.0.path.clone();
+                task::spawn(async move {
+                    let _ = fs::remove_file(path).await;
+                });
+            }
+        }
+    }
+}
 
-//         });
-//         self.manager.release_mem(self.capacity())
-//     }
-// }
 
-// #[derive(Clone)]
-// struct FileCache(Arc<CacheImpl>);
+#[derive(Clone)]
+struct FileCache(Arc<CacheImpl>);
 
-// #[derive(Clone)]
-// pub struct FileCacheGuard(Arc<FileCache>);
 
-// impl FileCacheGuard {
-//     pub fn new(manager: RawCacheManager, capacity: usize) -> Self {
-//         Self(Arc::new(CacheImpl {
-//             cache: RwLock::new(vec![0u8; capacity])
-//         }))
-//     }
-// }
+impl FileCache {
+    fn new(path: PathBuf, range: Range<u64>, to_remove: bool) -> Self {
+        let cache = Self(Arc::new(CacheImpl {
+            state: RwLock::new(CacheState::Creating(StateWaiter::new())), 
+            path,
+            range, 
+            to_remove
+        }));
+        
+        {
+            let cache = cache.clone();
+            task::spawn(async move {
+                let ret = cache.create().await;
 
-// #[async_trait::async_trait]
-// impl RawCache for FileCacheGuard {
-//     fn capacity(&self) -> usize {
-//         self.0.capacity()
-//     }
+                let new_state = match ret {
+                    Ok(file) => CacheState::Created(file), 
+                    Err(err) => CacheState::Error(err.code(), err.msg().to_owned())
+                };
+                let waiters = {
+                    let state = &mut *cache.0.state.write().unwrap();
+                    match state {
+                        CacheState::Creating(waiters) => {
+                            let waiters = waiters.transfer();
+                            *state = new_state;
+                            waiters
+                        },
+                        _ => unreachable!()
+                    }
+                };
+                
+                waiters.wake();
+            });
+        }
+        
+        cache
+    }
 
-//     fn clone_as_raw_cache(&self) -> Box<dyn RawCache> {
-//         Box::new(self.clone())
-//     }
+    async fn create(&self) -> BuckyResult<File> {
+        let mut file = File::open(self.path()).await?;
+        let offset = file.seek(SeekFrom::Start(self.0.range.start)).await?;
+        if offset == self.range().start {
+            Ok(file)
+        } else {
+            Err(BuckyError::new(BuckyErrorCode::InvalidData,"offset to range failed"))
+        }
+    }
 
-//     async fn async_reader(&self) -> BuckyResult<Box<dyn Unpin + Send + Sync + AsyncReadWithSeek>> {
-//         Ok(Box::new(SeekWrapper::new(self)))
-//     }
+    fn seek(&self, cur: usize, pos: SeekFrom) -> usize {
+        let capacity = self.capacity();
+        match pos {
+            SeekFrom::Start(offset) => capacity.min(offset as usize), 
+            SeekFrom::Current(offset) => {
+                let offset = (cur as i64) + offset;
+                let offset = offset.max(0);
+                capacity.min(offset as usize)
+            },
+            SeekFrom::End(offset) => {
+                let offset = (capacity as i64) + offset;
+                let offset = offset.max(0);
+                capacity.min(offset as usize)
+            }
+        }
+    }
 
-//     fn sync_reader(&self) -> BuckyResult<Box<dyn SyncReadWithSeek>> {
-//         Err(BuckyError::new(BuckyErrorCode::NotSupport, "file cache does not support sync reader"))
-//     }
+    fn path(&self) -> &Path {
+        self.0.path.as_path()
+    }
+
+    fn capacity(&self) -> usize {
+        (self.0.range.end - self.0.range.start) as usize
+    } 
+
+    fn range(&self) -> &Range<u64> {
+        &self.0.range
+    }
+
+    async fn wait_created(&self) -> BuckyResult<File> {
+        let (ret, waiter) = {
+            match &mut *self.0.state.write().unwrap() {
+                CacheState::Creating(waiters) => (None, Some(waiters.new_waiter())),
+                CacheState::Created(file) => (Some(Ok(file.clone())), None), 
+                CacheState::Error(err, msg) => (Some(Err(BuckyError::new(err.clone(), msg.clone()))), None)
+            }
+        };
+        if let Some(ret) = ret {
+            ret 
+        } else if let Some(waiter) = waiter {
+            StateWaiter::wait(waiter, || {
+                match &*self.0.state.read().unwrap() {
+                    CacheState::Creating(_) => unreachable!(),
+                    CacheState::Created(file) => Ok(file.clone()), 
+                    CacheState::Error(err, msg) => Err(BuckyError::new(*err, msg.clone()))
+                }
+            }).await
+        } else {
+            unreachable!()
+        }
+    }
+}
+
+
+pub struct FileCacheReader {
+    file: File, 
+    cache: FileCache, 
+    offset: usize
+}
+
+
+impl async_std::io::Seek for FileCacheReader {
+    fn poll_seek(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        pos: SeekFrom,
+    ) -> Poll<std::io::Result<u64>> {
+        let reader = self.get_mut();
+        let file_offset = reader.cache.seek(reader.offset, pos) as u64 + reader.cache.range().start;
+
+        let ret = async_std::io::Seek::poll_seek(Pin::new(&mut reader.file), cx, SeekFrom::Start(file_offset));
+
+        match ret {
+            Poll::Ready(ret) => {
+                match ret {
+                    Ok(file_offset) => {
+                        let offset = file_offset - reader.cache.range().start;
+                        reader.offset = offset as usize;
+                        Poll::Ready(Ok(offset))
+                    }, 
+                    Err(err) => Poll::Ready(Err(err))
+                } 
+            },
+            Poll::Pending => Poll::Pending
+        }
+    }
+}
+
+impl async_std::io::Read for FileCacheReader {
+    fn poll_read(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &mut [u8],
+        ) -> Poll<std::io::Result<usize>> {
+        let reader = self.get_mut();
+        let new_offset = reader.cache.seek(reader.offset, SeekFrom::Current(buf.len() as i64));
+        let cliped = &mut buf[0..new_offset - reader.offset];
+
+        let ret = async_std::io::Read::poll_read(Pin::new(&mut reader.file), cx, cliped);
+
+        match ret {
+            Poll::Ready(ret) => {
+                match ret {
+                    Ok(read) => {
+                        reader.offset += read;
+                        Poll::Ready(Ok(read))
+                    }, 
+                    Err(err) => Poll::Ready(Err(err))
+                }
+            },
+            Poll::Pending => Poll::Pending
+        }
+    }
+}
+
+impl AsyncReadWithSeek for FileCacheReader {}
+
+#[derive(Clone)]
+pub struct FileCacheGuard(Arc<FileCache>);
+
+impl FileCacheGuard {
+    pub fn new(path: PathBuf, range: Range<u64>, to_remove: bool) -> Self {
+        Self(Arc::new(FileCache::new(path, range, to_remove)))
+    }
+}
+
+#[async_trait::async_trait]
+impl RawCache for FileCacheGuard {
+    fn capacity(&self) -> usize {
+        self.0.capacity()
+    }
+
+    fn clone_as_raw_cache(&self) -> Box<dyn RawCache> {
+        Box::new(self.clone())
+    }
+
+    async fn async_reader(&self) -> BuckyResult<Box<dyn Unpin + Send + Sync + AsyncReadWithSeek>> {
+        let cache = &self.0;
+        let file = cache.wait_created().await?;
+        
+        Ok(Box::new(FileCacheReader {
+            file, 
+            cache: self.0.as_ref().clone(),
+            offset: 0
+        }))
+    }
+
+    fn sync_reader(&self) -> BuckyResult<Box<dyn SyncReadWithSeek>> {
+        Err(BuckyError::new(BuckyErrorCode::NotSupport, "file cache does not support sync reader"))
+    }
     
-//     async fn async_writer(&self) -> BuckyResult<Box<dyn  Unpin + Send + Sync + AsyncWriteWithSeek>> {
-//         Ok(Box::new(SeekWrapper::new(self)))
-//     }   
+    async fn async_writer(&self) -> BuckyResult<Box<dyn  Unpin + Send + Sync + AsyncWriteWithSeek>> {
+        Err(BuckyError::new(BuckyErrorCode::NotSupport, "file cache does not support sync reader"))
+    }   
 
-//     fn sync_writer(&self) -> BuckyResult<Box<dyn SyncWriteWithSeek>> {
-//         Err(BuckyError::new(BuckyErrorCode::NotSupport, "file cache does not support sync reader"))
-//     }
-// }
+    fn sync_writer(&self) -> BuckyResult<Box<dyn SyncWriteWithSeek>> {
+        Err(BuckyError::new(BuckyErrorCode::NotSupport, "file cache does not support sync reader"))
+    }
+}
 
 
--- src/component/cyfs-bdt/src/utils/mod.rs
+++ src/component/cyfs-bdt/src/utils/mod.rs
@@ -3,7 +3,6 @@ pub mod download;
 pub mod mem_tracker;
 pub mod mem_chunk_store;
 pub mod local_chunk_store;
-// pub mod ndn_request;
 
 pub use mem_tracker::MemTracker;
 pub use mem_chunk_store::MemChunkStore;
