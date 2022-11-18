0e9741def31cd6f283465a88fae3669c24382a11
zwong91 <yzbingchuan@gmail.com>
2022-11-18 12:07:21 +0800

    NDN cross zone get chunk case

--- src/tests/cyfs-stack-bench/src/bench/cross_zone_ndn_bench.rs
+++ src/tests/cyfs-stack-bench/src/bench/cross_zone_ndn_bench.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use async_trait::async_trait;
 use cyfs_core::{Text, TextObj};
-use crate::{Bench, OOD_DEC_ID, Stat};
+use crate::{Bench, OOD_DEC_ID, Stat, DEVICE_DEC_ID};
 use log::*;
 use cyfs_base::*;
 use cyfs_lib::*;
@@ -13,7 +13,6 @@ pub struct CrossZoneNDNBench {
     stack: SharedCyfsStack,
     target: Option<ObjectId>,
     stat: Arc<Stat>,
-    chunks: Vec<ChunkId>,
 }
 
 const LIST: [&str;1] = ["get-chunk"];
@@ -40,24 +39,21 @@ impl CrossZoneNDNBench {
             stack,
             target,
             stat,
-            chunks: Vec::with_capacity(run_times),
         })
     }
     async fn test(&mut self) -> BuckyResult<()> {
         info!("let cross ood add chunks...");
         let ids = self.add_chunks().await?;
         info!("cross ood add {} chunks", ids.len());
-        for id in &ids {
-            //self.test_get_chunk(id.clone()).await?;
+        for _i in 0..self.run_times {
+            for id in &ids {
+                self.test_get_chunk(&id.0, &id.1, &id.2, &id.3).await?;
+            }
         }
-
-        //info!("let cross ood remove objs...");
-        self.remove_chunks(ids).await?;
         Ok(())
     }
 
-
-    async fn add_chunks(&self) -> BuckyResult<Vec<ChunkId>> {
+    async fn add_chunks(&self) -> BuckyResult<Vec<(DirId, FileId, String, ChunkId)>> {
         let q = new_object("add_chunk", &self.run_times.to_string());
 
         let mut req = NONPostObjectOutputRequest::new_router(self.target.clone(), q.desc().calculate_id(), q.to_vec().unwrap());
@@ -68,25 +64,27 @@ impl CrossZoneNDNBench {
         let ret = self.stack.non_service().post_object(req.clone()).await?;
         let t = Text::clone_from_slice(&ret.object.unwrap().object_raw).unwrap();
         assert_eq!(t.header(), "finish");
-        let ids = Vec::<ChunkId>::clone_from_hex(t.value(), &mut vec![]).unwrap();
+        let ids = Vec::<(DirId, FileId, String, ChunkId)>::clone_from_hex(t.value(), &mut vec![]).unwrap();
         Ok(ids)
     }
 
-    async fn remove_chunks(&self, _ids: Vec<ChunkId>) -> BuckyResult<()> {
-        Ok(())
-    }
-
-    async fn test_get_chunk(&self, chunk_id: ChunkId) -> BuckyResult<()> {
+    async fn test_get_chunk(&self, _dir_id: &DirId, file_id: &FileId, _inner_path: &str, chunk_id: &ChunkId) -> BuckyResult<()> {
         let device_id = DeviceId::try_from(self.target.clone().unwrap()).unwrap();
         info!("begin test-get-chunk...");
-        let req = NDNGetDataRequest::new_router(
+        let mut req = NDNGetDataRequest::new_router(
             Some(device_id.clone().into()),
             chunk_id.object_id().to_owned(),
             None,
         );
 
-        //req.common.req_path = Some(RequestGlobalStatePath::new(Some(OOD_DEC_ID.clone()), Some(NDN_CHUNKS_PATH)).format_string());
-    
+        req.common.dec_id = Some(DEVICE_DEC_ID.clone());
+        req.common.req_path = None;
+        let referer_object = NDNDataRefererObject {
+            target: None,
+            object_id: file_id.object_id().to_owned(),
+            inner_path: None,
+        };
+        req.common.referer_object = vec![referer_object.clone()];
         info!(
             "will get chunk from device: chunk={}, device={}",
             chunk_id, device_id,
@@ -94,7 +92,7 @@ impl CrossZoneNDNBench {
         let begin = std::time::Instant::now();
 
         let resp = self.stack.ndn_service().get_data(req).await.unwrap();
-        assert_eq!(resp.object_id, chunk_id.object_id());
+        assert_eq!(resp.object_id, chunk_id.object_id().to_owned());
         self.stat.write(self.name(),"get-chunk", begin.elapsed().as_millis() as u64);
 
         Ok(())
--- src/tests/cyfs-stack-bench/src/post_service.rs
+++ src/tests/cyfs-stack-bench/src/post_service.rs
@@ -1,7 +1,9 @@
 use cyfs_base::*;
 use cyfs_core::{Text, TextObj};
+use async_std::io::prelude::*;
 use log::*;
 use cyfs_lib::*;
+use std::path::Path;
 use std::sync::Arc;
 use std::str::FromStr;
 use cyfs_util::EventListenerAsyncRoutine;
@@ -35,6 +37,29 @@ enum ServiceType {
     CrossZoneNonTest,
     CrossZoneRootStateTest,
     CrossZoneNdnTest,
+}
+
+
+async fn gen_all_random_file(local_path: &Path) {
+    if local_path.exists() {
+        assert!(local_path.is_file());
+        info!("will remove random file: {}", local_path.display());
+        std::fs::remove_file(&local_path).unwrap();
+    }
+
+    //info!("will gen random file: {}", local_path.display());
+
+    let mut opt = async_std::fs::OpenOptions::new();
+    opt.write(true).create(true).truncate(true);
+
+    let mut f = opt.open(&local_path).await.unwrap();
+
+    for _i in 0..64 {
+        let buf_k: Vec<u8> = (0..1024).map(|_| rand::random::<u8>()).collect();
+        f.write_all(&buf_k).await.unwrap();
+    }
+
+    f.flush().await.unwrap();
 }
 
 struct OnPostObject {
@@ -207,23 +232,119 @@ impl EventListenerAsyncRoutine<RouterHandlerPostObjectRequest, RouterHandlerPost
                     let value = object.header().parse::<usize>()?;
                     info!("generating test chunks...");
                     let mut ids = Vec::with_capacity(value);
-                    for _i in 0..value {
-                        let buf: Vec<u8> = (0..3000).map(|_| rand::random::<u8>()).collect();
-                        let chunk_id = ChunkId::calculate(&buf).await.unwrap();
-                
-                        let mut req = NDNPutDataRequest::new_with_buffer(
-                            NDNAPILevel::Router,
-                            chunk_id.object_id().to_owned(),
-                            buf.clone(),
-                        );
-                        req.common.target = Some(self.owner.cyfs_stack.local_device_id().into());
-                        req.common.req_path = Some(RequestGlobalStatePath::new(Some(OOD_DEC_ID.clone()), Some(NDN_CHUNKS_PATH)).format_string());
-                        if let Err(e) = self.owner.cyfs_stack.ndn_service().put_data(req).await {
-                            error!("put chunk error! {}", e);
-                            unreachable!();
-                        }
-                        ids.push(chunk_id);
+                    let data_dir = cyfs_util::get_app_data_dir("cyfs-stack-bench").join("cross_zone");
+                    if data_dir.exists() {
+                        std::fs::remove_dir_all(&data_dir).unwrap();
                     }
+                    (0..10).for_each(|i| {
+                        let name = format!("test{}", i);
+                        let dir = data_dir.join(&name);
+                        std::fs::create_dir_all(&dir).unwrap();
+                        (0..2).for_each(|i| {
+                            let name = format!("{}.log", i);
+                            let local_path = dir.join(&name);
+                            if local_path.exists() {
+                                std::fs::remove_file(&local_path).unwrap();
+                            }
+                
+                            async_std::task::block_on(gen_all_random_file(&local_path));
+                        })
+                    });
+                
+                    let req = TransPublishFileOutputRequest {
+                        common: NDNOutputRequestCommon {
+                            req_path: None,
+                            dec_id: Some(OOD_DEC_ID.to_owned()),
+                            level: Default::default(),
+                            target: None,
+                            referer_object: vec![],
+                            flags: 0,
+                        },
+                        owner: Default::default(),
+                
+                        // 文件的本地路径
+                        local_path: data_dir.clone(),
+                
+                        // chunk大小
+                        chunk_size: 1024 * 1024,
+                
+                        // 关联的dirs
+                        file_id: None,
+                        dirs: None,
+                    };
+                
+                    let ret = self.owner.cyfs_stack.trans().publish_file(&req).await;
+                    if ret.is_err() {
+                        error!("trans add_dir error! {}", ret.unwrap_err());
+                        unreachable!();
+                    }
+                
+                    let resp = ret.unwrap();
+                    info!("ndn add dir success! id={}", resp.file_id);
+                
+                    let object_map_id = ObjectMapId::try_from(&resp.file_id).unwrap();
+                
+                    let file_id_from_objectmap;
+                    {
+                        let object = {
+                            let mut req = NONGetObjectRequest::new_noc(
+                                object_map_id.object_id().to_owned(),
+                                Some("/test1/1.log".to_owned()),
+                            );
+                            req.common.req_path = Some("/tests/non_file".to_owned());
+                
+                            let resp = self.owner.cyfs_stack.non_service().get_object(req).await.unwrap();
+                            resp.object
+                        };
+                
+                        file_id_from_objectmap = FileId::try_from(&object.object_id).unwrap();
+                    }
+                
+                    // convert objectmap to dir object
+                    let dir_id;
+                    {
+                        let req = UtilBuildDirFromObjectMapOutputRequest {
+                            common: UtilOutputRequestCommon::default(),
+                            object_map_id: object_map_id.object_id().clone(),
+                            dir_type: BuildDirType::Zip,
+                        };
+                        let resp = self.owner.cyfs_stack.util().build_dir_from_object_map(req).await.unwrap();
+                        dir_id = resp.object_id;
+                    }
+                
+                    let dir_id = DirId::try_from(&dir_id).unwrap();
+                
+                    let inner_path = "/test1/1.log";
+                    let object = {
+                        let mut req = NONGetObjectRequest::new_noc(
+                            dir_id.object_id().to_owned(),
+                            Some(inner_path.to_owned()),
+                        );
+                        req.common.req_path = Some("/tests/non_file".to_owned());
+                
+                        let resp = self.owner.cyfs_stack.non_service().get_object(req).await.unwrap();
+                        resp.object
+                    };
+                
+                    let file_id = FileId::try_from(&object.object_id).unwrap();
+                    assert_eq!(file_id, file_id_from_objectmap);
+                
+                    // get a chunk from file for later test cases
+                    let chunk_list = object.object().as_file().body_expect("").content().inner_chunk_list().unwrap();
+                    assert!(chunk_list.len() > 0);
+                    info!("got file's chunk: file={}, chunk={}", file_id, chunk_list[0]);
+                
+                
+                    // change the file permisssions
+                    let access = AccessString::full_except_write();
+                    let mut update_req = NONUpdateObjectMetaRequest::new_router(
+                        None, file_id.object_id().clone(), Some(access),
+                    );
+                    update_req.common.dec_id = Some(OOD_DEC_ID.clone());
+
+                    self.owner.cyfs_stack.non_service().update_object_meta(update_req).await.unwrap();
+
+                    ids.push((dir_id, file_id, inner_path.to_owned(), chunk_list[0].clone()));
 
                     let mut answer = new_object("add_chunks", "finish");
                     *answer.body_mut_expect("").content_mut().value_mut() = ids.to_hex().unwrap();
@@ -282,6 +403,7 @@ impl TestService {
         stub.add_access(GlobalStatePathAccessItem::new_group(ROOT_STATE_CALL_PATH, None, None, Some(DEVICE_DEC_ID.clone()), AccessPermissions::Full as u8)).await.unwrap();
         stub.add_access(GlobalStatePathAccessItem::new_group(CYFS_CRYPTO_VIRTUAL_PATH, None, None, Some(DEVICE_DEC_ID.clone()), AccessPermissions::CallOnly as u8)).await.unwrap();
         stub.add_access(GlobalStatePathAccessItem::new_group(NDN_CALL_PATH, None, None, Some(DEVICE_DEC_ID.clone()), AccessPermissions::Full as u8)).await.unwrap();
+        stub.add_access(GlobalStatePathAccessItem::new_group(NDN_CHUNKS_PATH, None, None, Some(DEVICE_DEC_ID.clone()), AccessPermissions::ReadOnly as u8)).await.unwrap();
 
         let service = Arc::new(self);
 
@@ -347,4 +469,5 @@ impl TestService {
                 })))
             .unwrap();
     }
+
 }
