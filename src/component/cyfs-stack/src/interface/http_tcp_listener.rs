782a470b5b65b7c5ee3ff0058f4f950a8ede2017
liyaxing <liyaxing@buckyos.com>
2022-11-28 15:27:44 +0800

    Improve system-config.toml format

--- src/component/cyfs-stack/src/interface/http_tcp_listener.rs
+++ src/component/cyfs-stack/src/interface/http_tcp_listener.rs
@@ -6,8 +6,8 @@ use cyfs_lib::{BaseTcpListener, BaseTcpListenerHandler, RequestProtocol};
 use async_std::net::TcpStream;
 use async_trait::async_trait;
 use std::net::SocketAddr;
-use std::sync::Arc;
 use std::sync::atomic::AtomicU64;
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub(super) struct ObjectHttpTcpListener {
@@ -66,9 +66,7 @@ impl ObjectHttpTcpListener {
     }
 
     fn next_seq(&self) -> u64 {
-        self
-            .seq
-            .fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+        self.seq.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
     }
 
     pub fn get_listen(&self) -> String {
@@ -120,9 +118,9 @@ impl ObjectHttpTcpListener {
                             }
                         } else {
                             warn!(
-                            "tcp http request complete with error! status={}, peer={}, during={}ms, seq={}",
-                            status, peer_addr, during, seq,
-                        );
+                                "tcp http request complete with error! status={}, peer={}, during={}ms, seq={}",
+                                status, peer_addr, during, seq,
+                            );
                         }
 
                         Ok(resp)
@@ -144,9 +142,12 @@ impl ObjectHttpTcpListener {
         .await;
 
         if let Err(e) = ret {
-            error!(
+            warn!(
                 "tcp http accept error, err={}, addr={}, peer={}, during={}ms",
-                e, self.listen_url, peer_addr, begin.elapsed().as_millis(),
+                e,
+                self.listen_url,
+                peer_addr,
+                begin.elapsed().as_millis(),
             );
             // FIXME 一般是请求方直接断开导致的错误，是否需要判断并不再输出warn？
             //Err(BuckyError::from(e))
