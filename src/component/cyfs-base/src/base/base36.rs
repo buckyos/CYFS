b3b48eb0f98db4954c86f14b212f5dd02560f982
liyaxing <liyaxing@buckyos.com>
2022-12-07 21:27:27 +0800

    Fix a base36 codec bug

--- src/component/cyfs-base/src/base/base36.rs
+++ src/component/cyfs-base/src/base/base36.rs
@@ -9,7 +9,7 @@ pub trait FromBase36 {
     fn from_base36(&self) -> BuckyResult<Vec<u8>>;
 }
 
-const ALPHABET: &[u8] = b"0123456789abcdefghijklmnoqprstuvwxyz";
+const ALPHABET: &[u8] = b"0123456789abcdefghijklmnopqrstuvwxyz";
 
 impl ToBase36 for [u8] {
     fn to_base36(&self) -> String {
@@ -33,13 +33,7 @@ mod test {
 
     use crate::*;
 
-    #[test]
-    fn test() {
-        let id = ObjectId::default();
-        let v = id.as_slice().to_base36();
-        println!("{}", v);
-        
-        let s = "9cfBkPt8hb8rnaYJRZGsKc9ZS3ye3CixxCC13z9Ubswm";
+    fn test_convert(s: &str) -> (ObjectId, String) {
         let id = ObjectId::from_str(s).unwrap();
         let v = id.to_base36();
         println!("{}", v);
@@ -50,5 +44,24 @@ mod test {
 
         let id2 = ObjectId::from_base36(&v.to_uppercase()).unwrap();
         assert_eq!(id, id2);
+
+        (id, v)
+    }
+
+    #[test]
+    fn test() {
+        let id = ObjectId::default();
+        let v = id.as_slice().to_base36();
+        println!("{}", v);
+        
+        // let s = "9cfBkPt8hb8rnaYJRZGsKc9ZS3ye3CixxCC13z9Ubswm";
+        let s = "9tGpLNna8UVtPYCfV1LbRN2Bqa5G9vRBKhDhZiWjd7wA";
+        let (id1, id11) = test_convert(s);
+
+        let s = "9tGpLNna8P1hutR3y6i1gSGuosoLXLxa72HMrcEQnrgk";
+        let (id2, id22) = test_convert(s);
+
+        assert_ne!(id11, id22);
+        assert_ne!(id1, id2);
     }
 }
