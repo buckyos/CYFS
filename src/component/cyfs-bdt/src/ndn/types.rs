6a1f7ed145931b97892dbb8ca862c45d5c6fa5ff
tsukasa <puotosssa@live.com>
2023-01-14 15:46:44 +0800

    Rebase: rebase to 74-integrate-bdt-task-context-and-group

--- src/component/cyfs-bdt/src/ndn/types.rs
+++ src/component/cyfs-bdt/src/ndn/types.rs
@@ -548,9 +548,6 @@ pub trait NdnTask: Send + Sync {
     fn cancel_by_error(&self, _err: BuckyError) -> BuckyResult<NdnTaskControlState> {
         Ok(NdnTaskControlState::Normal)
     }
-    fn cancel_by_error(&self) -> BuckyResult<NdnTaskControlState> {
-        
-    }
     fn pause(&self) -> BuckyResult<NdnTaskControlState> {
         Ok(NdnTaskControlState::Normal)
     }
