c88a5e76224f608c51ea62d4d9aa4642756d3c04
liyaxing <liyaxing@buckyos.com>
2023-02-13 13:14:57 +0800

    Add object and name for meta in cyfs-stack

---
+++ src/component/cyfs-stack/src/meta/cache.rs
@@ -0,0 +1,83 @@
+use cyfs_base::*;
+use cyfs_debug::Mutex;
+
+use lru_time_cache::LruCache;
+use std::borrow::Borrow;
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct MetaCacheItem<Value> {
+    last_update_time: u64,
+    value: Value,
+}
+
+struct MetaMemoryCacheImpl<Key, Value>
+where
+    Key: Ord + Clone,
+{
+    list: LruCache<Key, MetaCacheItem<Value>>,
+}
+
+impl<Key, Value> MetaMemoryCacheImpl<Key, Value>
+where
+    Key: Ord + Clone,
+    Value: Clone,
+{
+    pub fn new(timeout_in_secs: u64) -> Self {
+        let list = LruCache::with_expiry_duration_and_capacity(
+            std::time::Duration::from_secs(timeout_in_secs),
+            256,
+        );
+
+        Self { list }
+    }
+
+    pub fn add(&mut self, key: Key, value: Value) {
+        let item = MetaCacheItem {
+            value,
+            last_update_time: bucky_time_now(),
+        };
+
+        self.list.insert(key, item);
+    }
+
+    pub fn get<Q: ?Sized>(&mut self, key: &Q) -> Option<Value>
+    where
+        Key: Borrow<Q>,
+        Q: Ord,
+    {
+        self.list.get(&key).map(|v| v.value.clone())
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct MetaMemoryCache<Key, Value>(Arc<Mutex<MetaMemoryCacheImpl<Key, Value>>>)
+where
+    Key: Ord + Clone;
+
+impl<Key, Value> MetaMemoryCache<Key, Value>
+where
+    Key: Ord + Clone,
+    Value: Clone,
+{
+    pub fn new(timeout_in_secs: u64) -> Self {
+        Self(Arc::new(Mutex::new(MetaMemoryCacheImpl::new(
+            timeout_in_secs,
+        ))))
+    }
+
+    pub fn add(&self, key: Key, value: Value) {
+        self.0.lock().unwrap().add(key, value)
+    }
+
+    pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<Value>
+    where
+        Key: Borrow<Q>,
+        Q: Ord,
+    {
+        self.0.lock().unwrap().get(key)
+    }
+}
+
+pub(crate) type MetaMemoryCacheForObject = MetaMemoryCache<ObjectId, Vec<u8>>;
+pub(crate) type MetaMemoryCacheForName = MetaMemoryCache<String, Option<(NameInfo, NameState)>>;
