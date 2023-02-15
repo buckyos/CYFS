12a5b3b8fc16939ce394f98ae640ffd34cf870e0
zhangzhen <zhangzhen@buckyos.com>
2023-02-15 21:19:34 +0800

    integrate GroupState, not finish

---
+++ src/component/cyfs-group/src/storage/engine/storage_engine.rs
@@ -0,0 +1,23 @@
+use cyfs_base::{BuckyResult, ObjectId};
+
+#[async_trait::async_trait]
+pub trait StorageWriter: Send + Sync + Drop {
+    async fn insert_prepares(&mut self, block_id: &ObjectId) -> BuckyResult<()>;
+    async fn insert_pre_commit(&mut self, block_id: &ObjectId, is_instead: bool)
+        -> BuckyResult<()>;
+    async fn push_commit(&mut self, height: u64, block_id: &ObjectId) -> BuckyResult<()>;
+    async fn remove_prepares(&mut self, block_ids: &[ObjectId]) -> BuckyResult<()>;
+    async fn push_proposals(
+        &mut self,
+        proposal_ids: &[ObjectId],
+        timestamp: Option<u64>,
+    ) -> BuckyResult<()>;
+
+    async fn set_last_vote_round(&mut self, round: u64) -> BuckyResult<()>;
+}
+
+#[async_trait::async_trait]
+pub trait StorageEngine {
+    async fn find_block_by_height(&self, height: u64) -> BuckyResult<ObjectId>;
+    // async fn is_proposal_finished(&self, proposal_id: &ObjectId) -> BuckyResult<bool>;
+}
