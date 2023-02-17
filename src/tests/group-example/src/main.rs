6ed655fabaa58d37683037624376cff667478d16
zhangzhen <zhangzhen@buckyos.com>
2023-02-17 19:49:08 +0800

    isolate for test example

--- src/tests/group-example/src/main.rs
+++ src/tests/group-example/src/main.rs
@@ -363,7 +363,7 @@ mod Common {
 
         let stack_param = CyfsStackParams {
             config: CyfsStackConfigParams {
-                isolate: Some(device.desc().object_id()),
+                isolate: Some(device.desc().object_id().to_string()),
                 sync_service: false,
                 shared_stack: false,
             },
