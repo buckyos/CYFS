c99b5e2a244bc3cd59ca2ec0d54eb513f3b699d8
zhangzhen <zhangzhen@buckyos.com>
2023-02-22 21:23:03 +0800

    prepare for verify

--- src/component/cyfs-core/src/group/mod.rs
+++ src/component/cyfs-core/src/group/mod.rs
@@ -3,7 +3,6 @@ mod group_proposal;
 mod group_proposal_decide_param;
 mod group_quorum_certificate;
 mod group_rpath;
-mod group_rpath_status;
 mod group_update_group_proposal_param;
 
 pub use group_consensus_block::*;
@@ -11,5 +10,4 @@ pub use group_proposal::*;
 pub use group_proposal_decide_param::*;
 pub use group_quorum_certificate::*;
 pub use group_rpath::*;
-pub use group_rpath_status::*;
 pub use group_update_group_proposal_param::*;
