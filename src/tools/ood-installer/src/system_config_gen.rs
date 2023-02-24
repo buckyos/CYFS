52994e810e04d8f22e446021f0fb9e49246e7eb1
liyaxing <liyaxing@buckyos.com>
2023-02-24 15:43:40 +0800

    Improve config with some comments

--- src/tools/ood-installer/src/system_config_gen.rs
+++ src/tools/ood-installer/src/system_config_gen.rs
@@ -4,6 +4,9 @@ use cyfs_base::{BuckyError, BuckyResult};
 const SYSTEM_CONFIG_TEMPLATE: &str = r#"[device]
 target = "${target}"
 config_desc = "${config_repo}"
+#service_list_version = "nightly"
+#service_version = "default"
+#preview = false
 
 [[repository]]
 type = "${repo_type}"
