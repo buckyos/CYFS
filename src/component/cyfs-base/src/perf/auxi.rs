142ad1351260682cc37db04d4bc3c78b5de807b6
liyaxing <liyaxing@buckyos.com>
2023-02-25 16:18:14 +0800

    Add scope relate perf helper macros

--- src/component/cyfs-base/src/perf/auxi.rs
+++ src/component/cyfs-base/src/perf/auxi.rs
@@ -20,6 +20,65 @@ macro_rules! declare_module_perf_isolate {
                 }
             }
         }
+
+        pub struct PerfScopeRequest<'a> {
+            id: &'a str,
+            key: String,
+        }
+        
+        impl<'a> PerfScopeRequest<'a> {
+            pub fn new(
+                id: &'a str,
+            ) -> Self {
+                let key = cyfs_base::perf_request_unique_id();
+                if let Some(ins) = crate::get_current_perf_instance() {
+                    ins.begin_request(id, &key);
+                }
+        
+                Self {
+                    id,
+                    key,
+                }
+            }
+        }
+        
+        impl<'a> Drop for PerfScopeRequest<'a> {
+            fn drop(&mut self) {
+                if let Some(ins) = crate::get_current_perf_instance() {
+                    ins.end_request(self.id, &self.key, cyfs_base::BuckyErrorCode::Ok, None);
+                }
+            }
+        }
+    }
+}
+
+use std::sync::atomic::{AtomicU64, Ordering};
+
+pub fn perf_request_unique_id() -> String {
+    static INDEX: AtomicU64 = AtomicU64::new(0);
+    let ret = INDEX.fetch_add(1, Ordering::SeqCst);
+    ret.to_string()
+}
+
+#[macro_export]
+macro_rules! perf_scope_request {
+    ( $id:expr, $block:block ) => {
+        if let Some(ins) = crate::get_current_perf_instance() {
+            let req_id = cyfs_base::perf_request_unique_id();
+            ins.begin_request($id, &req_id);
+            match $block {
+                Ok(v) => {
+                    ins.end_request($id, &req_id, cyfs_base::BuckyErrorCode::Ok, None);
+                    Ok(v)
+                }
+                Err(e) => {
+                    ins.end_request($id, &req_id, e.code(), None);
+                    Err(e)
+                }
+            }
+        } else {
+            $block
+        }
     }
 }
 
@@ -34,20 +93,49 @@ macro_rules! perf_begin_request {
 
 #[macro_export]
 macro_rules! perf_end_request {
+    ( $id:expr, $key:expr ) => {
+        if let Some(ins) = crate::get_current_perf_instance() {
+            ins.end_request($id, $key, cyfs_base::BuckyErrorCode::Ok, None);
+        }
+    };
+    ( $id:expr, $key:expr, $err:expr ) => {
+        if let Some(ins) = crate::get_current_perf_instance() {
+            ins.end_request($id, $key, $err, None);
+        }
+    };
     ( $id:expr, $key:expr, $err:expr, $bytes:expr ) => {
         if let Some(ins) = crate::get_current_perf_instance() {
             ins.end_request($id, $key, $err, $bytes);
         }
+    };
+}
+
+
+#[macro_export]
+macro_rules! perf_simple_scope_request {
+    ( $id:expr ) => {
+        let __perf_simple_scope_request = crate::PerfScopeRequest::new($id);
     }
 }
 
+
 #[macro_export]
 macro_rules! perf_acc {
+    ($id:expr) => {
+        if let Some(ins) = crate::get_current_perf_instance() {
+            ins.acc($id, cyfs_base::BuckyErrorCode::Ok, None);
+        }
+    };
+    ($id:expr, $err:expr) => {
+        if let Some(ins) = crate::get_current_perf_instance() {
+            ins.acc($id, $err, None);
+        }
+    };
     ( $id:expr, $err:expr, $size:expr ) => {
         if let Some(ins) = crate::get_current_perf_instance() {
             ins.acc($id, $err, $size);
         }
-    }
+    };
 }
 
 #[macro_export]
--- src/component/cyfs-base/src/perf/dummy.rs
+++ src/component/cyfs-base/src/perf/dummy.rs
@@ -4,17 +4,33 @@ macro_rules! declare_module_perf_isolate {
 }
 
 #[macro_export]
+macro_rules! perf_scope_request {
+    ( $id:expr, $block:block ) => {
+        $block
+    };
+}
+
+#[macro_export]
 macro_rules! perf_begin_request {
     ( $id:expr, $key:expr ) => {};
 }
 
 #[macro_export]
 macro_rules! perf_end_request {
+    ( $id:expr, $key:expr ) => {};
+    ( $id:expr, $key:expr, $err:expr ) => {};
     ( $id:expr, $key:expr, $err:expr, $bytes:expr ) => {};
 }
 
 #[macro_export]
+macro_rules! perf_simple_scope_request {
+    ( $id:expr ) => {};
+}
+
+#[macro_export]
 macro_rules! perf_acc {
+    ($id:expr) => {};
+    ($id:expr, $err:expr) => {};
     ( $id:expr, $err:expr, $size:expr ) => {};
 }
 
