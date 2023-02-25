8708a0ef603d4f1c0cda8c6e7ea255355ef1bb23
liyaxing <liyaxing@buckyos.com>
2023-02-25 12:15:03 +0800

    Add perf related definitions to cyfs-base

---
+++ src/component/cyfs-base/src/perf/dummy.rs
@@ -0,0 +1,29 @@
+#[macro_export]
+macro_rules! declare_module_perf_isolate {
+    ( $id:expr ) => {};
+}
+
+#[macro_export]
+macro_rules! perf_begin_request {
+    ( $id:expr, $key:expr ) => {};
+}
+
+#[macro_export]
+macro_rules! perf_end_request {
+    ( $id:expr, $key:expr, $err:expr, $bytes:expr ) => {};
+}
+
+#[macro_export]
+macro_rules! perf_acc {
+    ( $id:expr, $err:expr, $size:expr ) => {};
+}
+
+#[macro_export]
+macro_rules! perf_action {
+    ( $id:expr, $err:expr, $name:expr, $value:expr ) => {};
+}
+
+#[macro_export]
+macro_rules! perf_record {
+    ( $id:expr, $total:expr, $total_size:expr ) => {};
+}
