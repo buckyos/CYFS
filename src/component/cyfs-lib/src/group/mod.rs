7fdbff4bf6bcac420dec50f716f2cd382f222f93
zhangzhen <zhangzhen@buckyos.com>
2023-03-01 20:16:27 +0800

    Move the interface to cyfs-lib

--- src/component/cyfs-lib/src/group/mod.rs
+++ src/component/cyfs-lib/src/group/mod.rs
@@ -1,9 +1,7 @@
-// dec framework
-
+mod delegate_factory;
 mod group_manager;
 mod rpath_client;
-mod rpath_control;
 
+pub use delegate_factory::*;
 pub use group_manager::*;
 pub use rpath_client::*;
-pub use rpath_control::*;
