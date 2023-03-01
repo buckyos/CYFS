f3591ab3a53f3f3e3225192a1a6fe85e5cfa955e
zhangzhen <zhangzhen@buckyos.com>
2023-03-01 20:16:27 +0800

    Move the interface to cyfs-lib

---
+++ src/component/cyfs-lib/src/group/rpath_client.rs
@@ -0,0 +1,27 @@
+use std::sync::Arc;
+
+use cyfs_base::{
+    BuckyError, BuckyErrorCode, BuckyResult, GroupMemberScope, NamedObject, ObjectDesc, ObjectId,
+    RawConvertTo,
+};
+use cyfs_core::{GroupProposal, GroupRPath};
+
+use crate::NONObjectInfo;
+
+struct RPathClientRaw {}
+
+#[derive(Clone)]
+pub struct RPathClient(Arc<RPathClientRaw>);
+
+impl RPathClient {
+    pub fn rpath(&self) -> &GroupRPath {
+        unimplemented!()
+    }
+
+    pub async fn post_proposal(
+        &self,
+        proposal: &GroupProposal,
+    ) -> BuckyResult<Option<NONObjectInfo>> {
+        unimplemented!()
+    }
+}
