5477443dac5f8547055a59f23d27c84850c4fb80
liyaxing <liyaxing@buckyos.com>
2023-03-02 19:39:19 +0800

    Add chunk length verify to poll_read method in cache reader

--- src/component/cyfs-bdt-ext/src/cache/reader/cache.rs
+++ src/component/cyfs-bdt-ext/src/cache/reader/cache.rs
@@ -1,13 +1,12 @@
+use cyfs_base::*;
 use cyfs_bdt::{chunk::ChunkCache, DownloadTaskSplitRead};
 use cyfs_chunk_cache::ChunkManagerRef;
 use cyfs_chunk_lib::*;
-use cyfs_base::*;
 
 use async_std::io::{Read, Result};
 use std::io::{Seek, SeekFrom};
 use std::pin::Pin;
 use std::task::{Context, Poll};
-
 
 pub trait ChunkSplitReader: DownloadTaskSplitRead + Seek {}
 
@@ -19,15 +18,25 @@ pub struct ChunkListCacheReader {
 
     chunk_manager: ChunkManagerRef,
     last_cache_chunk: Option<ChunkId>,
+    full_len: u64,
+    read_len: u64,
 }
 
 impl ChunkListCacheReader {
     pub fn new(
         chunk_manager: ChunkManagerRef,
         task_id: String,
+        full_len: u64,
         reader: Box<dyn ChunkSplitReader + Unpin + Send + Sync + 'static>,
     ) -> Self {
-        Self { task_id, reader, chunk_manager, last_cache_chunk: None, }
+        Self {
+            task_id,
+            reader,
+            chunk_manager,
+            last_cache_chunk: None,
+            full_len,
+            read_len: 0,
+        }
     }
 
     fn try_cache_chunk(&mut self, cache: &ChunkCache) {
@@ -61,17 +70,30 @@ impl ChunkListCacheReader {
         async_std::task::spawn(async move {
             let ret = chunk_manager.exist(cache.chunk()).await;
             if ret {
-                debug!("cache chunk to chunk manager but already exists! chunk={}", cache.chunk());
+                debug!(
+                    "cache chunk to chunk manager but already exists! chunk={}",
+                    cache.chunk()
+                );
                 return;
             }
 
             let cache_wrapper = ChunkCacheWrapper::new(cache.clone());
-            match chunk_manager.put_chunk(cache.chunk(), Box::new(cache_wrapper)).await {
+            match chunk_manager
+                .put_chunk(cache.chunk(), Box::new(cache_wrapper))
+                .await
+            {
                 Ok(()) => {
-                    info!("cache chunk to chunk manager success! chunk={}", cache.chunk());
+                    info!(
+                        "cache chunk to chunk manager success! chunk={}",
+                        cache.chunk()
+                    );
                 }
                 Err(e) => {
-                    info!("cache chunk to chunk manager failed! chunk={}, {}", cache.chunk(), e);
+                    info!(
+                        "cache chunk to chunk manager failed! chunk={}, {}",
+                        cache.chunk(),
+                        e
+                    );
                 }
             }
         });
@@ -87,11 +109,26 @@ impl Read for ChunkListCacheReader {
         match Pin::new(self.reader.as_mut()).poll_split_read(cx, buf) {
             Poll::Ready(ret) => match ret {
                 Ok(Some((cache, range))) => {
+                    self.read_len += range.len() as u64;
                     self.try_cache_chunk(&cache);
                     Poll::Ready(Ok(range.len()))
                 }
-                Ok(None) => Poll::Ready(Ok(0)),
-                Err(e) => Poll::Ready(Err(e)),
+                Ok(None) => {
+                    if self.full_len != self.read_len {
+                        error!(
+                            "unmatch data size got from bdt: data={}, len={}, got={}",
+                            self.task_id, self.full_len, self.read_len
+                        );
+                    }
+                    Poll::Ready(Ok(0))
+                }
+                Err(e) => {
+                    warn!(
+                        "read data from bdt got error! data={}, len={}, {}",
+                        self.task_id, self.full_len, e
+                    );
+                    Poll::Ready(Err(e))
+                }
             },
             Poll::Pending => Poll::Pending,
         }
@@ -99,10 +136,7 @@ impl Read for ChunkListCacheReader {
 }
 
 impl Seek for ChunkListCacheReader {
-    fn seek(
-        self: &mut Self,
-        pos: SeekFrom,
-    ) -> std::io::Result<u64> {
+    fn seek(self: &mut Self, pos: SeekFrom) -> std::io::Result<u64> {
         Pin::new(self.reader.as_mut()).seek(pos)
     }
 }
@@ -149,7 +183,8 @@ impl Chunk for ChunkCacheWrapper {
     }
 
     async fn read(&mut self, buf: &mut [u8]) -> BuckyResult<usize> {
-        let len = self.cache
+        let len = self
+            .cache
             .read(self.offset, buf, || std::future::pending())
             .await?;
         self.offset += len;
