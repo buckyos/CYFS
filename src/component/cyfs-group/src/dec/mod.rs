7db18c25cab487a4145d2aa2c496c9785aed34c7
zhangzhen <zhangzhen@buckyos.com>
2023-03-03 14:56:29 +0800

    Rename `control` into `Service`

--- src/component/cyfs-group/src/dec/mod.rs
+++ src/component/cyfs-group/src/dec/mod.rs
@@ -3,9 +3,9 @@
 mod group_events;
 mod group_manager;
 mod rpath_client;
-mod rpath_control;
+mod rpath_service;
 
 pub use group_events::*;
 pub use group_manager::*;
 pub use rpath_client::*;
-pub use rpath_control::*;
+pub use rpath_service::*;
